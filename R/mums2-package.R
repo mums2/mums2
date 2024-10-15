2b2253227f0f22f3cb670be43bacb6fbabfb2890
amason30 <masonar@umich.edu>
2024-10-15 15:31:51 -0400

    add convert_metaboscape2mass_dataset function

--- R/mums2-package.R
+++ R/mums2-package.R
@@ -1,3 +1,13 @@
 ## usethis namespace: start
+#' @importFrom cli cli_abort
+#' @importFrom dplyr case_when
+#' @importFrom dplyr ends_with
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr rename_all
+#' @importFrom dplyr select
+#' @importFrom magrittr %>%
+#' @importFrom massdataset create_mass_dataset
+#' @importFrom tibble column_to_rownames
 #' @useDynLib mums2, .registration = TRUE
 ## usethis namespace: end
