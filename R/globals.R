d95bce149056be28aeb2c793c33b6993ec1f4eaa
Allison R Mason <31289802+amason30@users.noreply.github.com>
2024-10-15 15:52:07 -0400

    Data import (#1)
    * add convert_metaboscape2mass_dataset function
    
    * add unit tests - all tests and CMD passing

---
+++ R/globals.R
@@ -0,0 +1,8 @@
+utils::globalVariables(c("MASS", "adduct", "charge",
+                         "feature_id", "pepmass", "read.csv",
+                         "variable_id"))
+
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
