159f67f7f61ffec6535be441522b2ccfd19a4d3c
amason30 <masonar@umich.edu>
2024-10-16 10:38:18 -0400

    start score_ms2 - add accessory functions and their unit tests

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Get file paths for examples
+#'
+#' mums2 contains a number of example files in the `inst/extdata` directory.
+#' This function makes them accessible in documentation that shows how file
+#' paths are used in function examples.
+#'
+#' @param file Name of a file. If `NULL`, all examples files will be listed.
+#'
+#' @export
+#' @return A file path to example data stored in the `inst/extdata` directory
+#' of the package.
+#' @examples
+#' example()
+#'
+#' example("PSUMSMLS_Adenine.csv")
+example <- function(file = NULL) {
+  path <- ""
+  if (is.null(file)) {
+    path <- dir(system.file("extdata", package = "mums2"))
+  } else {
+    path <- system.file("extdata", file, package = "mums2", mustWork = TRUE)
+  }
+  return(path)
+}
---
+++ tests/testthat/test-score_ms2.R
@@ -0,0 +1,42 @@
+test_that("score_ms2 works on class peaks_data", {
+
+  dir <- "exttestdata"
+  file1 <- "MSV000086713_32.csv"
+  file2 <- "PSUMSMLS_Adenine.csv"
+
+  scan_32 <- create_peaks_data(read.csv(test_path(dir, file1)),
+                               136.0615)
+
+  adenine <- create_peaks_data(read.csv(test_path(dir, file2)),
+                               136.0620)
+
+  score <- score_ms2(scan_32, adenine, gnps_param(frag_tolerance = 0.5))
+  expect_equal(score, 0.81162792)
+})
+
+test_that("score_ms2 works on class peaks_data, method entropy", {
+
+  dir <- "exttestdata"
+  file1 <- "MSV000086713_32.csv"
+  file2 <- "PSUMSMLS_Adenine.csv"
+
+  scan_32 <- create_peaks_data(read.csv(test_path(dir, file1)),
+                               136.0615)
+
+  adenine <- create_peaks_data(read.csv(test_path(dir, file2)),
+                               136.0620)
+
+  e <- msentropy::msentropy_similarity(as.matrix(scan_32$spectra),
+                                       as.matrix(adenine$spectra),
+                                       ms2_tolerance_in_da = 0.02,
+                                       ms2_tolerance_in_ppm = -1,
+                                       clean_spectra = TRUE,
+                                       min_mz = 0,
+                                       max_mz = 1000,
+                                       noise_threshold = 0.01,
+                                       max_peak_num = 100,
+                                       weighted = TRUE)
+
+  score <- score_ms2(scan_32, adenine, spec_entropy_params())
+  expect_equal(score, e)
+})
