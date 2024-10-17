a5943971ae033b65d684aa93cbbac609c65acb40
Allison R Mason <31289802+amason30@users.noreply.github.com>
2024-10-17 16:33:29 -0400

    Add dist_ms2 (#4)
    * optimize dist_ms2 with rcpp - cutoff is not applied correctly
    
    * added cutoff to cpp function - all tests pass and output matches original R output
    
    * cpp version of dist_ms2 performs 3x faster
    
    * use cpp function for dist_ms2

---
+++ R/dist_ms2.R
@@ -0,0 +1,63 @@
+#' Calculate pairwise distance between MS/MS features.
+#'
+#' @description
+#' `dist_ms2` calculates and stores all non-zero distance values above
+#'  the user defined cutoff (default = 0.3).
+#'
+#' @details
+#' This function takes a mass_dataset as input and calculates distance
+#'  between ms2 peaks. Currently, MS1 features without MS2 peaks returns
+#'  no distance value. Distance can be calculated with method `"gnps"`
+#'  or `"spectral_entropy"`. A sparse matrix is returned.
+#'
+#'
+#' @param data An object containing MA2 data.
+#' @param cutoff The maximum distance value (`numeric`) to store a pairwise
+#'  comparison. The default of .3 corresponds to a cosine score of .7,
+#'  meaning pairs with a score of .7 or higher will be stored in the matrix.
+#' @param precursor_thresh Precursor mz tolerance. MS2 scans with a
+#'  difference in precursor mz less than or equal to this value will be scored.
+#' @param score_params Parameters for scoring method to be applied.
+#'  See [gnps_params()] and [spec_entropy_params()] for more details.
+#'
+#' @return A sparse matrix of class `"data.frame"`
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(dplyr)
+#'
+#' dat <- tidyMassDemo
+#' first_5 <- c("M71T776_POS", "M72T54_POS", "M81T51_POS",
+#'    "M83T51_1_POS", "M83T51_2_POS")
+#'
+#' dat_sub <- dat %>%
+#'    massdataset::activate_mass_dataset("variable_info") %>%
+#'    massdataset::filter(variable_id %in% first_5)
+#'
+#' dat_sub_dist <- dist_ms2(dat_sub, 0.3, 2, gnps_params(0.5))}
+dist_ms2 <- function(data, cutoff, precursor_thresh, score_params) {
+  UseMethod("dist_ms2", data)
+}
+
+#' @method dist_ms2 mass_dataset
+#' @export
+dist_ms2.mass_dataset <- function(data, cutoff, precursor_thresh, score_params) {
+  data_list <- list("pmz" = data@ms2_data[[1]]@ms2_mz,
+                    "id" = data@ms2_data[[1]]@variable_id,
+                    "spectra" = lapply(data@ms2_data[[1]]@ms2_spectra,
+                                       as.data.frame))
+
+  dist <- distMS2(data_list, score_params, precursor_thresh, cutoff)
+
+  return(dist)
+}
+
+
+# is_same_scan <- function(spectra_1, spectra_2) {
+#   if (nrow(spectra_1) != nrow(sepectra_2)) {
+#     return(FALSE)
+#   }
+
+#   return(TRUE)
+# }
---
+++ src/Distance.cpp
@@ -0,0 +1,57 @@
+#include "Distance/Distance.h"
+
+
+void Distance::CreateSpectraList(Rcpp::List data) {
+    std::vector<double> pmz =  Rcpp::as<std::vector<double>>(data["pmz"]);
+    std::vector<std::string> name =  Rcpp::as<std::vector<std::string>>(data["id"]);
+    Rcpp::List mzInts = data["spectra"];
+    
+    const int n = pmz.size();
+    for(int i = 0; i < n; i++){
+        Rcpp::DataFrame specDataFrame = Rcpp::wrap(mzInts[i]);
+        Spectra spec(name[i], specDataFrame["mz"], specDataFrame["intensity"], pmz[i]);
+        spectraList.emplace_back(spec);
+    }
+}
+
+void Distance::CalculateDistances(const double prec_threshold, double cutoff, const ScoringFactory& scoreMethod) {
+    const size_t size = spectraList.size();
+    for(int i = 0; i < size; i++) {
+        Spectra firstSpectra = spectraList[i];
+        for(int j = i + 1; j < size; j++){
+            Spectra secondSpectra = spectraList[j];
+            
+            if (std::abs(firstSpectra.precursorMz - secondSpectra.precursorMz) > prec_threshold) {
+                continue;
+            }
+            double score = scoreMethod.CalculateScore(firstSpectra, secondSpectra);
+
+            if ((1 - score) > cutoff) {
+                continue;
+            }
+            
+            SparseValue dist(i, j, (1 - score));
+            sparseMatrix.emplace_back(dist);
+        }
+    }
+} 
+
+Rcpp::DataFrame Distance::ExtractMatrix() {
+    int len = sparseMatrix.size();
+    Rcpp::IntegerVector i(len);
+    Rcpp::IntegerVector j(len);
+    Rcpp::NumericVector dist(len);
+    
+    for (int p = 0; p < len; p++) {
+        i[p] = sparseMatrix[p].getI() + 1;
+        j[p] = sparseMatrix[p].getJ() + 1;
+        dist[p] = sparseMatrix[p].getDistance();
+    }
+
+    Rcpp::DataFrame m = Rcpp::DataFrame::create(Rcpp::Named("i") = i,
+                                                Rcpp::Named("j") = j,
+                                                Rcpp::Named("dist") = dist);
+
+    return m;
+    
+}
