9bc01c481521d3cdd645b46580781ab72dc5ca92
GregJohnsonJr <gregjohn456@gmail.com>
2025-03-21 12:03:06 -0400

    Removing unnecessary files and documenting function's properly. The check is done to only 1 note!

--- R/community_object.R
+++ R/community_object.R
@@ -2,7 +2,7 @@
 #' @title Create Community Matrix
 #' @description
 #' Takes the shared dataframe from clustur and converts it into a community matrix object
-#' 
+#' @param cluster_object the result of the `cluster_data()` function.
 create_community_matrix_object <- function(cluster_object) {
   df <- get_abundance(cluster_object)
   samples <- unique(df$samples)
@@ -23,7 +23,7 @@ create_community_matrix_object <- function(cluster_object) {
 #' @title Get Community Matrix
 #' @description
 #' Returns the community `matrix` or the data that you used to create the object.
-#' 
+#' @param community_object the object created from the `create_community_object()` function.
 get_community_matrix <- function(community_object) {
   if(!("community_object" %in% class(community_object))) {
     stop("Please ensure the community_object is created from the `create_community_object` function.")
@@ -35,9 +35,10 @@ get_community_matrix <- function(community_object) {
 #' @title Print Community Object
 #' @description
 #' 3 function for print the community object
-#' 
+#' @param x the object created from the `create_community_object()` function.
+#' @param ... other parameters that are included in the print function.
 print.community_object <- function(x, ...) {
-  print(get_community_matrix(x))
+  print(get_community_matrix(x), ...)
 }
 
 
