c1e5b9f0f328661fa020c4222aa84970aa014ed0
YoungKrug <gregjohn456@gmail.com>
2025-03-26 12:09:41 -0400

    Added base skeleton for mpactr wrapper functions

---
+++ R/filter.R
@@ -0,0 +1,77 @@
+# Base Filter Function
+
+#' @export
+filter_peak_table <- function(params) {
+  return(UseMethod("filter"))
+}
+
+# Filter dispatch functions
+#' @export
+filter.filter_mispicked_ions <- function(params) {
+  return(filter_mispicked_ions(mpactr_object = params$mpactr_object,
+  ringwin = params$ringwin, isowin = params$isowin,
+  trwin = params$trwin, max_iso_shift = params$max_iso_shift,
+  merge_peaks = params$merge_peaks, merge_method = parms$merge_method,
+  copy_object = params$copy_object
+  ))
+}
+
+#' @export
+filter.filter_group <- function(params, ...) {
+  return(filter_group(mpactr_object = params$mpactr_object, group_threshold = params$group_threshold,
+  group_to_remove = params$group_to_remove, remove_ions = params$remove_ions,
+  copy_object = params$copy_object))
+}
+
+#' @export
+filter.filter_cv <- function(params) {
+  return(filter_cv(mpactr_object = params$mpactr_object, cv_threshold = params$cv_threshold,
+  cv_param = params$cv_param, copy_object = params$copy_object))
+}
+
+#' @export
+filter.filter_insource_ions <- function(params) {
+  return(filter_insource_ions(mpactr_object = params$mpactr_object,
+                              cluster_threshold = params$cluster_threshold,
+                              copy_object = params$copy_object))
+}
+
+# Filter Parameter Getters
+
+filter_mispicked_ions_parameters <- function(mpactr_object, ringwin = 0.5, isowin = 0.01, 
+                                             trwin = 0.005, max_iso_shift = 3, 
+                                             merge_peaks = TRUE, merge_method = "sum", 
+                                             copy_object = FALSE) {
+  params <- list(mpactr_object = mpactr_object, ringwin = ringwin,
+                 isowin = isowin, trwin = trwin, max_iso_shift = max_iso_shift,
+                 merge_peaks = merge_peaks, merge_method = merge_method,
+                 copy_object = copy_object)
+  class(params) <- "filter_mispicked_ions"
+  return(params)
+}
+
+filer_group_parameters <- function(mpactr_object, group_threshold = 0.01, group_to_remove,
+                                   remove_ions = TRUE, copy_object = FALSE) {
+  params <- list(mpactr_object = mpactr_object, group_threshold = group_threshold,
+    group_to_remove = group_to_remove, remove_ions = remove_ions,
+    copy_object = copy_object)
+  class(params) <- "filter_group"
+  return(params)
+}
+
+filter_cv_parameters <- function(mpactr_object, cv_threshold = NULL, cv_param, copy_object = FALSE) {
+  params <- list(mpactr_object = mpactr_object, cv_threshold = cv_threshold,
+    cv_param = cv_param, copy_object = copy_object)
+  
+  class(params) <- "filter_cv"
+  return(params)
+}
+
+filter_insource_ions_parameters <- function(mpactr_object, cluster_threshold = 0.95, copy_object = FALSE) {
+  params <- list(mpactr_object = mpactr_object,
+                cluster_threshold = cluster_threshold,
+                copy_object = copy_object)
+  class(params) <- "filter_insource_ions"
+  return(params)
+}
+
---
+++ R/import.R
@@ -0,0 +1,4 @@
+import_all_data <- function(peak_table, meta_data, format) {
+  return(import_data(peak_table = peak_table,
+                     meta_data = meta_data, format = format))
+}
