8b376ae9692f7ae06bb2f7173acbb8fa999e8117
YoungKrug <gregjohn456@gmail.com>
2025-04-09 14:58:07 -0400

    Modifying the cluster function so it only needs to take a mass_data object as input.

--- R/cluster.R
+++ R/cluster.R
@@ -4,16 +4,15 @@
 #' Clusters the data together
 #' @param distance_df a distance df that was generated from the `distMs2()` function
 #' @param ms2_match_data your mass data set object
-#' @param mpactr_object your mass data set object
 #' @param cluster_method a cluster method, there are five methods to choose from:
 #' furthest, nearest, weighted, average, and opticlust. opticlust is the default
 #' 
-cluster_data <- function(distance_df, ms2_match_data, mpactr_object, cluster_method = "opticlust") {
+cluster_data <- function(distance_df, ms2_match_data, cluster_method = "opticlust") {
 
   sparse_matrix <- create_sparse_matrix(distance_df$i, distance_df$j, distance_df$dist)
 
   # Create Count Table 
-  count_table <- create_count_table(ms2_match_data, mpactr_object)
+  count_table <- create_count_table(ms2_match_data)
 
   # Create Distance Object
   dist <- read_dist(sparse_matrix, count_table, 0.3, F)
