2b2c1d65ea90a512706d63b7c0f5bad68f009622
GregJohnsonJr <gregjohn456@gmail.com>
2025-04-14 11:45:02 -0400

    Separating alpha and beta diversity dissimilarity functions.

--- R/diversity_metrics.R
+++ R/diversity_metrics.R
@@ -11,20 +11,49 @@ diversity <- function(community_object, diversity_index){
   CalculateDiversityCommunityObject(community_object, diversity_index)
 }
 
-#' @title avgDist with Community object
+#' @title Distance Shared
 #' @export
-#' @description avgdist
+#' @description dissimiliarty via beta diversity
 #' @param community_object the object created from the `create_community_object()` function.
 #' @param size the size you wish to rarefy your diversity matrix to.
 #' @param threshold the threshold you want your species to reach before it is included
 #' in the rarefaction sum.
-#' @param diversity_index the diversity index you wish to calculate diversity, the three options are
-#' shannon, simpson, and bray.
-#' @param iterations the amount of times you wish to run your alpha or beta diversity metrics.
-averaged_subsampled_dissimilarity <- function(community_object, size, threshold, diversity_index = "bray", iterations = 1000) {
+#' @param diversity_index the diversity index you wish to calculate diversity, the two options are bray.
+#' @param iterations the amount of times you wish to run your diversity metrics.
+dist_shared <- function(community_object, size, threshold, diversity_index = "bray", iterations = 1000) {
+  diversity_index_list <- c("bray")
   if(!("community_object" %in% class(community_object))) {
     stop("Please ensure the community_object is created from the `create_community_object` function.")
+  }
+  if(!(diversity_index %in% diversity_index_list)){
+    stop(paste0("Please ensure your diversity index is one of the following values: ",
+                paste(diversity_index_list, collapse = ' '))
+    )
   }
   return(FasterAvgDist(community_object, diversity_index, size, threshold, iterations))
 }
 
+
+#' @title Alpha Diversity Summary
+#' @export
+#' @description alpha diversity
+#' @param community_object the object created from the `create_community_object()` function.
+#' @param size the size you wish to rarefy your diversity matrix to.
+#' @param threshold the threshold you want your species to reach before it is included
+#' in the rarefaction sum.
+#' @param diversity_index the diversity index you wish to calculate diversity, the two options are
+#' shannon or simpson.
+#' @param iterations the amount of times you wish to run your diversity metrics.
+alpha_summary <- function(community_object, size, threshold, diversity_index = "shannon", iterations = 1000) {
+  diversity_index_list <- c("shannon", "simpson")
+  if(!("community_object" %in% class(community_object))) {
+    stop("Please ensure the community_object is created from the `create_community_object` function.")
+  }
+
+  if(!(diversity_index %in% diversity_index_list)){
+    stop(paste0("Please ensure your diversity index is one of the following values: ",
+                paste(diversity_index_list, collapse = ' '))
+    )
+  }
+  return(FasterAvgDist(community_object, diversity_index, size, threshold, iterations))
+}
