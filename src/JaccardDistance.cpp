10581c6308796008098f0f0236981941c05ebc48
GregJohnsonJr <gregjohn456@gmail.com>
2025-04-25 13:21:46 -0400

    Pushing the fix for jaccard calculations and ensuring that they can be called via the `diversity()` function.

--- src/JaccardDistance.cpp
+++ src/JaccardDistance.cpp
@@ -7,12 +7,21 @@
 double JaccardDistance::Calculate(const Rcpp::List& abundanceVectors) const {
     const Rcpp::NumericVector& sampleOne = abundanceVectors[0];
     const Rcpp::NumericVector& sampleTwo = abundanceVectors[1];
-    double sumOfAllOmus = Rcpp::sum(sampleOne) + Rcpp::sum(sampleTwo);
-    double amountOfSpeciesInBothSamples = 0;
-    for(int i = 0; i < abundanceVectors.size(); i++) {
-        if(sampleOne[i] > 0 && sampleTwo[i] > 0) {
-            amountOfSpeciesInBothSamples += sampleOne[i] + sampleTwo[i];
+    double sharedOmus = 0;
+    double sampleOneOmus = 0;
+    double sampleTwoOmus = 0;
+    for(int i = 0; i < sampleOne.size(); i++) {
+        double firstCurrentOmu = sampleOne[i];
+        double secondCurrentOmu = sampleTwo[i];
+        if(firstCurrentOmu > 0 && secondCurrentOmu > 0) {
+            sharedOmus++;
+            sampleOneOmus++;
+            sampleTwoOmus++;
+            continue;
         }
+        if(firstCurrentOmu > 0) sampleOneOmus++;
+        if(secondCurrentOmu > 0) sampleTwoOmus++;
+
     }
-    return amountOfSpeciesInBothSamples/sumOfAllOmus;
+    return 1 - sharedOmus / (sampleOneOmus + sampleTwoOmus - sharedOmus);
 }
