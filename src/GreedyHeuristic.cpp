6b64765c9c9f959a6829a151c675a8df5d56604e
YoungKrug <gregjohn456@gmail.com>
2025-06-02 17:18:01 -0400

    Fix for fragmentation trees and greedy huerstic crashing problem. Also changed the sorting from score to index, the lower the index the higher the score of the color and since the first indexes are the parent indexes, they will occupy the front.

--- src/GreedyHeuristic.cpp
+++ src/GreedyHeuristic.cpp
@@ -9,11 +9,8 @@ std::string GreedyHeuristic::CalculateHeuristic(FragmentationTree& tree) {
     const std::vector<FragmentationNode>& nodes = tree.GetFragmentationNodes();
     //Get all nodes of color 0
     size_t eligibleCandidateCounter = 0;
-    while (nodes[eligibleCandidateCounter].color == 0) {
-        eligibleCandidateCounter++;
+    for (size_t i = 0; i < nodes.size(); ++i) {
+        if (nodes[i].color != 0) continue;
+        return nodes[i].formula.GetMolecularFormula();
     }
-    // If there is no subRoot, (which shouldn't be possible since there should not be circular sub-molecules)
-    // then choose the highest score
-    const FragmentationNode& candidate = nodes[0];
-    return candidate.formula.GetMolecularFormula();
 }
