71c0fc8b1be87481bb5b82bf088b33049e1ed19a
YoungKrug <gregjohn456@gmail.com>
2025-06-04 15:28:22 -0400

    Removing comments

--- src/GreedyHeuristic.cpp
+++ src/GreedyHeuristic.cpp
@@ -5,8 +5,6 @@
 #include "FragmentationTree/GreedyHeuristic.h"
 
 std::string GreedyHeuristic::CalculateHeuristic(FragmentationTree& tree) {
-    // In worse case scenario, return the first node, which will always
-    // be a node of the parent index
     tree.SortFragmentationNodes();
     return tree.GetFragmentationNodes()[0].formula.GetMolecularFormula();
 }
