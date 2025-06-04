d1166a12c4db94702d1fea9a88d3dca9d90bcdce
YoungKrug <gregjohn456@gmail.com>
2025-06-04 15:02:24 -0400

    Updating a reverting code from the greedy heurstic function.

--- src/GreedyHeuristic.cpp
+++ src/GreedyHeuristic.cpp
@@ -8,24 +8,5 @@ std::string GreedyHeuristic::CalculateHeuristic(FragmentationTree& tree) {
     // In worse case scenario, return the first node, which will always
     // be a node of the parent index
     tree.SortFragmentationNodes();
-
-    const std::vector<FragmentationNode>& nodes = tree.GetFragmentationNodes();
-    const double maxScore = nodes[0].subTreeScore;
-    size_t chosenIndex = 0;
-    for (size_t i = 1; i < nodes.size(); ++i) {
-        if (nodes[i].subTreeScore < maxScore) {
-            break;
-        }
-        if (nodes[i].index < nodes[chosenIndex].index) chosenIndex = i;
-    }
-    Rcpp::Rcout << "Chosen Index: " << chosenIndex << std::endl;
-    //Get all nodes of color 0
-    // for (const auto & node : nodes) {
-    //     if (node.color != 0) continue;
-    //     return node.formula.GetMolecularFormula();
-    // }
-    // Should be unreachable
-    return tree.GetFragmentationNodes()[chosenIndex].formula.GetMolecularFormula();
-    // Rcpp::warning("Returning empty molecular formula. ");
-    // return "";
+    return tree.GetFragmentationNodes()[0].formula.GetMolecularFormula();
 }
