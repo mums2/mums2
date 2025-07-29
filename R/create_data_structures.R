ce525081593bb9eec1aebd84b21bcf2a8ece4f40
GregJohnsonJr <gregjohn456@gmail.com>
2025-07-29 17:10:40 -0400

    Added a function to convert matched data, into a matched data object with group averages. Allowing for samples with triplicates to be averaged out.

--- R/create_data_structures.R
+++ R/create_data_structures.R
@@ -42,6 +42,42 @@ create_community_matrix <- function(cluster_object) {
   return(combined_df)
 }
 
+#' @export
+#' @title Convert Samples to Group Averages
+#' @description
+#' To account for users measuring there data in triplicates or other forms of measurement,
+#' we have implemented a function that can tranform your matched data object to use
+#' group averages instead of each sample individually. 
+#' @param matched_data your mass data set object generated from `ms2_ms1_compare()`.
+#' @param mpactr_object The object created from `import_all_data()`.
+#' @examples 
+#' data <- import_all_data(peak_table = mums2::mums2_example("full_mix_peak_table.csv"), 
+#'                             meta_data = mums2::mums2_example("full_mix_meta_data.csv"), 
+#'                              format = "Metaboscape")
+#'
+#' filtered_data <- data |>
+#'    filter_peak_table(filter_mispicked_ions_parameters()) |>
+#'    filter_peak_table(filter_cv_parameters(cv_threshold = 0.2)) |>
+#'    filter_peak_table(filter_group_parameters(group_threshold = 0.1, "Blanks")) |>
+#'    filter_peak_table(filter_insource_ions_parameters())
+#'
+#' change_rt_to_seconds_or_minutes(filtered_data, "minutes")
+#' 
+#' matched_data <- ms2_ms1_compare(mums2_example("full_mix_ms2.mgf"),
+#'  filtered_data, 2, 6)
+#' 
+#' matched_data_avg <- convert_samples_to_group_averages(matched_data, filtered_data)
+#' 
+convert_samples_to_group_averages <- function(matched_data, mpactr_object) {
+  trips <- t(get_triplicate_averages(mpactr_object, matched_data))
+  meta_data <- get_meta_data(mpactr_object)
+  injection_samples <- meta_data$Injection
+  modifed_peak_table <- matched_data$ms1_data[,which(!(colnames(matched_data$ms1_data) %in% injection_samples)), with = FALSE]
+  matched_data$ms1_data <- cbind(modifed_peak_table, trips)
+  matched_data$samples <- unique(meta_data$Sample_Code)
+  return(matched_data)
+}
+
 # Helper function for creating count tables
 create_count_table <- function(ms2_match_data) {
   ms2_matches_compounds <- ms2_match_data$ms2_matches$ms1_compound_id
@@ -50,4 +86,21 @@ create_count_table <- function(ms2_match_data) {
   samples <- peak_table[which(peak_table$Compound %in% ms2_matches_compounds), ]
   return(data.frame(Representative_Sequence = samples$Compound, 
                     total = rowSums(samples[,-1]), samples[,-1], check.names = FALSE))
+}
+
+
+# Helper function for getting a matrix that displays the average of the triplicates
+get_triplicate_averages <- function(mpactr_data, matched_data) {
+  compounds <- matched_data$ms2_matches$ms1_compound_id
+  peak <- get_peak_table(mpactr_data)
+  meta_data <- get_meta_data(mpactr_data)
+  sample_codes <- unique(meta_data$Sample_Code)
+  triplicate_averages <- matrix(0, nrow(peak), 0)
+  rownames(triplicate_averages) <- peak$Compound
+  for(sample in sample_codes) {
+    means <- as.matrix(rowMeans(peak[, meta_data$Injection[which(meta_data$Sample_Code == sample)], with = FALSE]))
+    triplicate_averages <- cbind(triplicate_averages, means)
   }
+  colnames(triplicate_averages) <- sample_codes
+  return(t(triplicate_averages))
+}
