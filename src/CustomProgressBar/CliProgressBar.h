aeff35b60db7e5e59cea3f3ab753c5eec5c11f37
YoungKrug <gregjohn456@gmail.com>
2025-10-01 12:27:01 -0400

    Fix for "Conditional jump or move depends on uninitialised value(s)" Valgrind issue.

--- src/CustomProgressBar/CliProgressBar.h
+++ src/CustomProgressBar/CliProgressBar.h
@@ -73,7 +73,7 @@ protected:
 
         std::string time_string = _time_to_string(rem_time);
         if (_firstTime)
-            time_string = "-";
+            time_string6 = "-";
         for (int i = 0; i < _ticks_displayed; ++i) {
             RcppThread::Rcout << "\033[32m\u25A0" << std::flush;
         }
@@ -117,6 +117,6 @@ private:
     int _ticks_displayed;  // the nb of ticks already displayed
     bool _finalized;
     bool _firstTime = true;
-    time_t start, end;
+    time_t start{}, end{};
 };
 #endif //CLIPROGRESSBAR_H
