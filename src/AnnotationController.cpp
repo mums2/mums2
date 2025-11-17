ff1dd8c543e9019c69d468a449837ad309c5dbb3
YoungKrug <gregjohn456@gmail.com>
2025-11-17 16:18:19 -0500

    Added threading to annotations!

--- src/AnnotationController.cpp
+++ src/AnnotationController.cpp
@@ -26,7 +26,8 @@ std::vector<AnnotationNodeData> AnnotationController::GetNodes(const std::vector
     }
     return nodes;
 }
-
+// [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::depends(RcppThread)]]
 std::queue<AnnotatedNode> AnnotationController::AnnotateFeature(const std::vector<Feature> &features,
 const ScoringFactory& factory, const double minScoreThreshold, const double chemicalMinScore,
 const double precursorThreshold, const size_t minPeaks) const {
@@ -34,22 +35,31 @@ const double precursorThreshold, const size_t minPeaks) const {
     std::queue<AnnotatedNode> result;
     CliProgressBar progressBar;
     int currentProgress = 0;
+    const int size = annotations.size();
     const auto maxProgress = static_cast<float>(features.size());
+    std::mutex mutex;
     for (const auto& feature: features) {
-        for (const auto& node : annotations) {
-            if ((std::abs(feature.mz - node.precursorMz)) * 1e6 / feature.mz > precursorThreshold) continue;
-            const double chemicalSimilarity = MolecularFormulaSimilarity::ComputeSimilarity2(feature.formula,
-                node.chemicalFormula);
-            if (chemicalSimilarity < chemicalMinScore) continue;
-            const double score = factory.CalculateScore(feature.spectra, node.spectra, minPeaks);
-            if (score < minScoreThreshold) continue;
-            AnnotatedNode annotatedNode;
-            annotatedNode.feature = feature;
-            annotatedNode.node = node;
-            annotatedNode.formulaSimilarity = chemicalSimilarity;
-            annotatedNode.score = score;
-            result.push(annotatedNode);
-        }
+        RcppThread::parallelFor(0, size, [this, &feature, &factory, &result,
+            &minScoreThreshold, &chemicalMinScore, &precursorThreshold, &minPeaks, &mutex](int i) {
+            const AnnotationNodeData& node = annotations[i];
+            if ((std::abs(feature.mz - node.precursorMz)) * 1e6 / feature.mz <= precursorThreshold) {
+                const double chemicalSimilarity = MolecularFormulaSimilarity::ComputeSimilarity2(feature.formula,
+               node.chemicalFormula);
+                if (chemicalSimilarity >= chemicalMinScore) {
+                    const double score = factory.CalculateScore(feature.spectra, node.spectra, minPeaks);
+                    if (score >= minScoreThreshold) {
+                        AnnotatedNode annotatedNode;
+                        annotatedNode.feature = feature;
+                        annotatedNode.node = node;
+                        annotatedNode.formulaSimilarity = chemicalSimilarity;
+                        annotatedNode.score = score;
+                        mutex.lock();
+                        result.push(annotatedNode);
+                        mutex.unlock();
+                    }
+                }
+            }
+        }, 12);
         progressBar.update(static_cast<float>(currentProgress++)/maxProgress);
     }
     progressBar.end_display();
