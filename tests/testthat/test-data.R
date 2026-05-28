ab10a4e4c0cb2f62df594fda9043b9d2d0674880
GregJohnsonJr <gregjohn456@gmail.com>
2026-05-28 16:46:45 -0400

    Removing a datafile that is not being used

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -5,6 +5,6 @@ test_that("mums2_example returns proper paths to test data", {
                 class(mums2_example("boryillus_peaktable.csv")))
   expect_true("character" %in% class(mums2_example("botryllus_v2.gnps.mgf")))
   expect_true("character" %in% class(mums2_example()))
-  expect_true(length(mums2_example()) == 8)
+  expect_true(length(mums2_example()) == 7)
   expect_error(mums2_example("dat"))
 })
